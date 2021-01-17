9ec0b900e9350f51d35031bbb58b5bf239f745ca
Alicja <48097077+Turowska@users.noreply.github.com>
2021-01-17 23:10:45 +0100

    przykładowa metoda w Widoku

--- src/view/View.java
+++ src/view/View.java
@@ -1,4 +1,13 @@
 package view;
 
+import controler;
+
 public class View {
+  private Controler controler;
+  public View(Controler kontroler){
+    controler = kontroler;
+  }
+  
+  public oferta(String tabOfert[]){
+  }
 }
