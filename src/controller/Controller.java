50ece466f954850d55f9f0c665e4a6ae9d8c9891
Alicja <alicjalom@wp.pl>
2021-01-21 13:55:06 +0100

    zmiana w kontrolerze

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -7,12 +7,15 @@ import view.View;
 public class Controller {
     private Model model;
     private View view;
+    
+    private OknoLogowaniaController logowanie;
 
     public Controller (Model model, View view)
     {
         this.model = model; this.view = view;
         view.setControllerAndModel(this, model);
         //przekazaï¿½ do modelu view i this
+        logowanie = new OknoLogowaniaController();
         start();
     }
 
@@ -21,21 +24,17 @@ public class Controller {
     }
     
     public void probaLogowania(String name, String surname) {
-    	if(checkIfEmployeeExists(name, surname)) {
-    		//sprawdz czy menedzer
-    	}
+    	if(logowanie.checkIfEmployeeExists(name, surname)) {
+    		//stwórz pracownika
+    		Pracownik employee /*= */;
+    		if(logowanie.checkIfEmployeeIsManager(employee)) {
+    			view.oknoStartoweMenadzer();
+    		}
+    		else {
+    			view.oknoStartoweZwyklegoPracownika();
+    		}
+    	} else {
     	view.oknoLogowaniePonowne();
-    }
-    
-    private boolean checkIfEmployeeExists(String name, String surname){
-        name = name.toLowerCase();
-        name = name.substring(0,1).toUpperCase() + name.substring(1);
-        surname = surname.toLowerCase();
-        surname = surname.substring(0,1).toUpperCase() + surname.substring(1);
-        DatabaseController db = new DatabaseController();
-        Pracownik employee = db.selectPracownik(name, surname);
-        if(employee != null)
-            return true;
-        return false;
+    	}
     }
 }
