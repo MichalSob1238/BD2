d4f1cdec6c83364d6214ff054170668fe4d707a9
Alicja <alicjalom@wp.pl>
2021-01-25 09:37:56 +0100

    formularz Faktury, początek

---
+++ src/view/fakturaFormularz.java
@@ -0,0 +1,31 @@
+package view;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class fakturaFormularz extends JPanel implements ActionListener{
+	
+	private View view;
+	
+	private JLabel name;
+	private JButton cofnij;
+	private GridBagLayout layout;
+	private GridBagConstraints pom;
+	
+	private JLabel tytul;
+	private JButton zatwierdz;
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
